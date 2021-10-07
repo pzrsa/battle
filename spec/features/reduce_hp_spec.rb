f6f6153ff754e110f4fe099257f270b5d40e16e7
parsa <parsamesg@gmail.com>
2021-10-07 16:08:37 +0100

    fix to go back to home page after attacking

--- spec/features/reduce_hp_spec.rb
+++ spec/features/reduce_hp_spec.rb
@@ -2,6 +2,7 @@ feature "Reduce HP" do
   scenario "reduce player 2 HP by 10 points" do
     sign_in_and_play
     click_button("Attack")
+    click_button("Go back")
     expect(page).to have_content("John: 50 HP & Bob: 40 HP")
   end
 end
